34bff239514122c1db51713c4ac6e9abb2c9ec2b
Marco Jiménez Sandi <68356804+marcojm0690@users.noreply.github.com>
2023-03-08 19:06:53 -0600

    Add header

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HeaderComponent } from 'src/shared/header.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,HeaderComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
