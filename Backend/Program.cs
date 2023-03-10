116e7d2832212816d333e3a4d52636715ecf89a4
Marco Jiménez Sandi <68356804+marcojm0690@users.noreply.github.com>
2023-03-10 10:49:42 -0600

    Add connection string to dbContext

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -13,7 +13,9 @@ namespace Backend
 
             builder.Services.AddControllers();
             builder.Services.AddDbContext<PruebaContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("dbconn")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("PruebaDB")));
+
+
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
